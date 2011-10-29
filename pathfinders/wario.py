655bfd5cb428aef15f3c2b9c0a0bb1996ed685ce
lif <lifning@localhost>
2011-10-29 14:40:36 -0400

    made some changes to Wario. it can now beat Super Mario Bros. world 1-1 (with SuperOpti's granularity set to 10)

--- pathfinders/wario.py
+++ pathfinders/wario.py
@@ -10,8 +10,7 @@ import pygame
 from skeleton_solver import Brain
 
 defaultargs = { 'lookahead':    3,
-				'maxlookahead': 7,
-				'increasetime': 2 }
+				'escapedepth':  5 }
 
 class Wario(Brain):
 	name = 'wario'
@@ -20,17 +19,14 @@ class Wario(Brain):
 		Brain.__init__(self, game, args, defaultargs)
 
 		self.lookahead = self.args['lookahead']
-		self.maxlookahead = self.args['maxlookahead']
-		self.increasetime = self.args['increasetime']
+		self.escapedepth = self.args['escapedepth']
 
 		self.input_log = []
 		self.new_best = None
 		self.terminated = False
 
 		self._GenInputLists(self.lookahead)
-		self.frames_since_increase = 0
 
-		#self.game.Input(0) # hack for emulator to kick into gear
 		self.current_state = self.game.Freeze()
 		self.current_heur = self.game.Heuristic()
 		self.escape_heur = self.current_heur
@@ -39,9 +35,9 @@ class Wario(Brain):
 	def _GenInputLists(self, max, path = []):
 		if len(path) == 0:
 			self.input_substrings = []
-		if len(path) >= max:
+		else:
 			self.input_substrings.append(path)
-		elif not self.terminated:
+		if len(path) < max:
 			for i in self.game.ValidInputs():
 				self._GenInputLists(max, path + [i])
 
@@ -55,20 +51,23 @@ class Wario(Brain):
 			return True
 		return False
 
+	def _Escape_DLS(self, depth = 0):
+		if depth < self.escapedepth:
+			state = self.game.Freeze()
+			for i in self.input_substrings:
+				self.game.Thaw(state)
+				for j in i:  self.game.Input(j)
+				if self.game.Heuristic() < self.escape_heur:
+					return i
+				result = self._Escape_DLS(depth+1)
+				if result is not None:
+					return i + result
+		return None
+		
+
 	def Step(self):
-		# see if we're done trying to escape a local minimum
-		if self.frames_since_increase > 0:
-			self.frames_since_increase += 1
-			if self.frames_since_increase > self.increasetime:
-				if self.escape_heur == self.current_heur:
-					print 'Wario: could not escape from local minimum.'
-					self.terminated = True
-					return
-				print 'Wario: reverting to a lookahead of', self.lookahead
-				self._GenInputLists(self.lookahead)
-				self.frames_since_increase = 0
-		# otherwise, see if rerunning the last sequence will work again
-		elif self.new_best is not None:
+		# see if rerunning the last sequence will work again
+		if self.new_best is not None:
 			self.game.Thaw(self.current_state)
 			if self._RunString(self.new_best):
 				print self.new_best, 'immagonnaween'
@@ -77,31 +76,30 @@ class Wario(Brain):
 				return
 			print self.new_best
 
-		# finally, find things
+		# otherwise, find things
 		start_state = self.current_state
 		self.new_best = None
 		for i in self.input_substrings:
 			if self.terminated:  break
 			self.game.Thaw(start_state)
-			result = self._RunString(i)
-			if result and self.frames_since_increase > 0:
-				print 'Wario: seemingly escaped from local minimum'
-				self._GenInputLists(self.lookahead)
-				self.frames_since_increase = 0
-				break
+			self._RunString(i)
 			yield self.game.Draw()
 
-		if self.new_best is None:
-			if self.frames_since_increase > 0:
-				print 'Wario: got stuck in a local minimum or was unable to make visible progress.'
+		# see if we're stuck in a local min
+		if self.new_best is None and not self.terminated:
+			print 'Wario: temporarily stuck, depth-limited search to escape...'
+			self.escape_heur = self.current_heur
+			result = self._Escape_DLS()
+			if result is None:
+				print 'Wario: stuck in a local minimum or unable to see any progress.'
 				self.terminated = True
-			elif not self.terminated:
-				print 'Wario: temporarily stuck, increasing lookahead to', self.maxlookahead
-				self._GenInputLists(self.maxlookahead)
-				self.escape_heur = self.current_heur
-				self.frames_since_increase = 1
-		else:
-			self.input_log += self.new_best
+				self.new_best = []
+			else:
+				print 'Wario: found an escape.'
+				self.game.Thaw(start_state)
+				self._RunString(result)
+
+		self.input_log += self.new_best
 
 	def Event(self, evt):
 		if evt.type == pygame.QUIT:  self.terminated = True
