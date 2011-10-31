dc9ec63dbb89c04c6db5e940e87e0e5a3e2c5e57
lif <lifning@localhost>
2011-10-31 13:03:12 -0400

    recording and granularity in rerun, for making runs watchable

--- pathfinders/rerun.py
+++ pathfinders/rerun.py
@@ -11,7 +11,9 @@ from skeleton_solver import Brain
 
 defaultargs = { 'fps': 60, # run at 60fps because we have a human watching
 				'file': 'inputstring.pickle',
-				'force': False }
+				'granularity': 1, # mostly for converting SuperOpti runs to 60fps
+				'force': False,
+				'record': False }
 
 class Rerun(Brain):
 	name = 'rerun'
@@ -22,6 +24,8 @@ class Rerun(Brain):
 		self.clock = pygame.time.Clock()
 		self.fps = self.args['fps']
 		self.force = self.args['force']
+		self.granularity = self.args['granularity']
+		self.record = self.args['record']
 
 		loadedfile = cPickle.load(open(self.args['file'], 'r'))
 
@@ -42,9 +46,16 @@ class Rerun(Brain):
 		self.clock.tick(self.fps)
 		frameinput = 0
 		if len(self.inputstring): frameinput = self.inputstring.pop(0)
-		self.game.Input(frameinput)
-		self.outputstring.append(frameinput)
-		return (self.game.Draw(),)
+		for i in xrange(self.granularity):
+			self.game.Input(frameinput)
+			self.outputstring.append(frameinput)
+			surf = self.game.Draw()
+			if self.record:
+				pygame.image.save( surf,
+								   '%s_%s_%04d.png' % ( self.__class__.name,
+														self.game.__class__.name,
+														len(self.outputstring) ) )
+			yield surf
 
 	def Path(self):
 		return self.outputstring
