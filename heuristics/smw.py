46161348d385edb38b0e29971d12225c96cee34b
lif <lifning@localhost>
2011-11-19 14:07:00 -0500

    more improvements to SMW heuristic.  level width is no longer hard-coded

--- heuristics/smw.py
+++ heuristics/smw.py
@@ -7,27 +7,30 @@ By Darren Alton
 
 def Heuristic(self):
 	dead = float('inf')
-	level_end = 4830
+
 	level_ground_ypos = 380
 	mario_dead_anim = 9
+
+	level_screen_count = self._Byte(0x05D)
+	level_finished_timer = self._Byte(0x1493)  # this byte is set to 255 when the goal is reached
 
 	mario_x = self._Word(0xD2,0xD1)
 	mario_y = self._Word(0xD4,0xD3)
 	mario_anim = self._Byte(0x71)
+	mario_dash_timer = self._Byte(0x13E4)
 
 	#mario_xvel = self._Byte(0x7B)
 	#mario_yvel = self._Byte(0x7D)
 	#mario_xvel_fractional = self._Byte(0x7A)
-	mario_dash_timer = self._Byte(0x13E4)
+
+	# this should be a general estimate of where the goal tape is for most levels
+	level_goal_position = (level_screen_count-1)*256
 
 	if mario_anim == mario_dead_anim or mario_y > level_ground_ypos:
 		return dead
 
-	if mario_x > level_end:
+	# are we in the end-of-level marching sequence?
+	if level_finished_timer > 0:
 		return 0
 
-	return (level_end - mario_x) + (0x70 - mario_dash_timer)
-
-def Tiebreaker(self):
-	mario_dash_timer = self._Byte(0x13E4)
-	return mario_dash_timer
+	return (level_goal_position - mario_x) + (0x70 - mario_dash_timer)
