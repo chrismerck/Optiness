c912e09835543477df233405211f3f2ebef2cf57
lif <lifning@localhost>
2011-11-24 20:25:38 -0500

    made pickle_info recursive for some reason

--- output/pickle_info.py
+++ output/pickle_info.py
@@ -1,15 +1,17 @@
 from sys import argv
 from cPickle import load
 
-if __name__ == "__main__" and len(argv) > 1:
-	p = load(open(argv[1]))
+def show_data(p, keys=[]):
 	if type(p) == dict:
-		if len(argv) > 2:
-			for i in argv[2:]:
-				print repr(p[i])
+		if len(keys):
+			if len(keys) == 1 and keys[0] == '.':
+				print '{', '\n  '.join([ '{}:\t{}'.format(i, repr(p[i])) for i in p ]), '}'
+			else:
+				show_data(p[keys[0]], keys[1:])
 		else:
-			print 'keys:',
-			for i in p:
-				print i,
+			print 'keys: ', ' '.join(p)
 	else:
 		print p
+
+if __name__ == "__main__" and len(argv) > 1:
+	show_data( load( open(argv[1]) ), argv[2:] )
